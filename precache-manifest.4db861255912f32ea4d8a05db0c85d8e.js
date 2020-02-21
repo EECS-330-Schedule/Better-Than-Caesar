d7437f7996eb1435a66e0dbae7e860dc4ffee228
Virgil Chen <virgilchen97@gmail.com>
2020-02-21 00:18:38 -0600

    Updates

---
+++ precache-manifest.4db861255912f32ea4d8a05db0c85d8e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b834e908db9c83f265e39204145aaa70",
+    "url": "/better-than-caesar/index.html"
+  },
+  {
+    "revision": "0e33f155776e22952d26",
+    "url": "/better-than-caesar/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "1f162b46c27956e35c2c",
+    "url": "/better-than-caesar/static/js/2.34e5e1d9.chunk.js"
+  },
+  {
+    "revision": "3adc01bea76e7956dc3633ee898f6936",
+    "url": "/better-than-caesar/static/js/2.34e5e1d9.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0e33f155776e22952d26",
+    "url": "/better-than-caesar/static/js/main.3ebc0a3a.chunk.js"
+  },
+  {
+    "revision": "fcddc6103e1ee8c53eb9",
+    "url": "/better-than-caesar/static/js/runtime-main.2de3c0b3.js"
+  }
+]);
