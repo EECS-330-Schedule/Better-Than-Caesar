8578b40792cc1b2e869fdb212e242f75436cc335
Joshua <153996013@qq.com>
2020-02-26 14:15:15 -0600

    ~

--- src/components/Calendar.js
+++ src/components/Calendar.js
@@ -7,74 +7,107 @@ import SearchBox from './SearchBox';
 
 
 
+const Calendar=()=>{
 
+  const fs = require('fs');
+  const readline = require('readline');
+  console.log(readline);
+  const {google} = require('googleapis');
 
+  // If modifying these scopes, delete token.json.
+  const SCOPES = ['https://www.googleapis.com/auth/calendar.readonly'];
+  // The file token.json stores the user's access and refresh tokens, and is
+  // created automatically when the authorization flow completes for the first
+  // time.
+  const TOKEN_PATH = 'token.json';
 
-export default class Calendar extends React.Component {
-  constructor(props){
-  super(props);
-  this.weekday=["Monday", "Tuesday", "Wednesday", "Thursday", "Friday"];
-  this.hour=["8:00","9:00","10:00","11:00","12:00","13:00","14:00","15:00","16:00",
-            "17:00","18:00","19:00","20:00","21:00","22:00"]
+  // Load client secrets from a local file.
+  fs.readFile('./src/credentials.json', (err, content) => {
+    console.log(err);
+    if (err) return console.log('Error loading client secret file:', err);
+    // Authorize a client with credentials, then call the Google Calendar API.
+    authorize(JSON.parse(content), listEvents);
+  });
+
+  /**
+   * Create an OAuth2 client with the given credentials, and then execute the
+   * given callback function.
+   * @param {Object} credentials The authorization client credentials.
+   * @param {function} callback The callback to call with the authorized client.
+   */
+   
+  function authorize(credentials, callback) {
+    const {client_secret, client_id, redirect_uris} = credentials.installed;
+    const oAuth2Client = new google.auth.OAuth2(
+        client_id, client_secret, redirect_uris[0]);
+
+    // Check if we have previously stored a token.
+    fs.readFile(TOKEN_PATH, (err, token) => {
+      if (err) return getAccessToken(oAuth2Client, callback);
+      oAuth2Client.setCredentials(JSON.parse(token));
+      callback(oAuth2Client);
+    });
   }
 
   
+    //* Get and store new token after prompting for user authorization, and then
+    //* execute the given callback with the authorized OAuth2 client.
+    //* @param {google.auth.OAuth2} oAuth2Client The OAuth2 client to get token for.
+    //* @param {getEventsCallback} callback The callback for the authorized client.
+   
+  function getAccessToken(oAuth2Client, callback) {
+    const authUrl = oAuth2Client.generateAuthUrl({
+      access_type: 'offline',
+      scope: SCOPES,
+    });
+    console.log('Authorize this app by visiting this url:', authUrl);
+    const rl = readline.createInterface({
+      input: process.stdin,
+      output: process.stdout,
+    });
+    rl.question('Enter the code from that page here: ', (code) => {
+      rl.close();
+      oAuth2Client.getToken(code, (err, token) => {
+        if (err) return console.error('Error retrieving access token', err);
+        oAuth2Client.setCredentials(token);
+        // Store the token to disk for later program executions
+        fs.writeFile(TOKEN_PATH, JSON.stringify(token), (err) => {
+          if (err) return console.error(err);
+          console.log('Token stored to', TOKEN_PATH);
+        });
+        callback(oAuth2Client);
+      });
+    });
+  }
 
-  render(){
-    function Day(props){
-      return (<th>
-                    <span class="long">{props.day}</span>
-                  </th>);
-    }
+  /**
+   * Lists the next 10 events on the user's primary calendar.
+   * @param {google.auth.OAuth2} auth An authorized OAuth2 client.
+   */
 
-    function Cell(props){
-      return <li class="cell" id={props.day+props.hour}></li>;
-    }
-
-    function Hour(props){
-      var weekday=["Monday", "Tuesday", "Wednesday", "Thursday", "Friday"]
-      return <ul id={props.hour}>
-              <li> {props.hour} </li>
-              {weekday.map( (day)=> <Cell day={day} hour={props.hour}/> )}
-            </ul>
-    }
-
-
-    // var element_head=this.weekday.map((day) => <Day day={day}/> 
-    // );
-
-    // var week=this.weekday.map((day)=> <Day day={day}/>)
-
-    // var element_body=this.hour.map((hour)=><Hour hour={hour}/> )
-
-    return (<div class="calendar">
-              <div class="body">
-                <div class="lightgrey body-list">
-                  <ul class="week_header">
-                    <li> </li>
-                    <li>MON</li>
-                    <li>TUE</li>
-                    <li>WED</li>
-                    <li>THU</li>
-                    <li>FRI</li>
-                  </ul>
-                </div>
-                <div class="darkgrey body-list">
-                  {this.hour.map( (hour)=> <Hour hour={hour} /> )}
-                </div>
-              </div>
-            </div>)
-
-    }
+   
+  function listEvents(auth) {
+    const calendar = google.calendar({version: 'v3', auth});
+    calendar.events.list({
+      calendarId: 'primary',
+      timeMin: (new Date()).toISOString(),
+      maxResults: 10,
+      singleEvents: true,
+      orderBy: 'startTime',
+    }, (err, res) => {
+      if (err) return console.log('The API returned an error: ' + err);
+      const events = res.data.items;
+      if (events.length) {
+        console.log('Upcoming 10 events:');
+        events.map((event, i) => {
+          const start = event.start.dateTime || event.start.date;
+          console.log(`${start} - ${event.summary}`);
+        });
+      } else {
+        console.log('No upcoming events found.');
+      }
+    });
+  }
 }
 
-
-
-
-
-
-
-
-
-
-
+export default Calendar;
